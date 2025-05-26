1789c33408db0e9470c20e63b5d12cb3c7aa7622
Keith <keithlutes@gmail.com>
2025-05-26 12:24:15 -0600

    trying a command fix for Jenkinsfile job build_3.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-# 1. validate args: expect exactly 3: <model-dir> --output-file <output-json>
+# 1. validate args: expect exactly 3: <model-dir> --output <output-json>
 if [ "$#" -ne 3 ]; then
-	echo "Usage: $0 <model-dir> <policy-file> --output-file <output-json>"
+	echo "Usage: $0 <model-dir> <policy-file> --output <output-json>"
 	exit 1
 fi
 
 # 2. extract params
 MODEL_DIR=$1
-if [ "$2" != "--output-file" ]; then # || [ -z "${2-}" ]; then
-	echo "Error: expected --output-file <output-json>"
+if [ "$2" != "--output" ]; then # || [ -z "${2-}" ]; then
+	echo "Usage: $(basename "$0") <model-dir> --output <output-json>"
+	echo "also...Error: expected --output <output-json>"
 	exit 1
 fi
 OUTPUT_JSON=$3
 
 # run modelscan
-modelscan scan -p "$MODEL_DIR" -r json --output-file "$OUTPUT_JSON"
+modelscan scan -p "$MODEL_DIR" -r json --output "$OUTPUT_JSON"
 
 echo "Scan complete. Report written to '$OUTPUT_JSON'."
